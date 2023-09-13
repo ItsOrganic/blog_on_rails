bb98804d0a1fc2ec6adf46d26a1d6e42e27ffbf5
unknown <stutyashivam@gmail.com>
2023-09-13 21:12:25 +0530

    added the show.htm.erb to display results using id

--- blog/app/controllers/blog_posts_controller.rb
+++ blog/app/controllers/blog_posts_controller.rb
@@ -2,4 +2,7 @@ class BlogPostsController < ApplicationController
     def index
     @blog_posts = BlogPosts.all()
     end
+    def show
+        @blog_post = BlogPosts.find(params[:id])
+    end
 end
