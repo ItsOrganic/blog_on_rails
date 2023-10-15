207b98f597755d9c21c13e303c59e42c42c6a2d2
unknown <stutyashivam@gmail.com>
2023-10-15 12:43:24 +0530

    fixed

--- blog/app/models/blog_posts.rb
+++ blog/app/models/blog_posts.rb
@@ -2,9 +2,9 @@ class BlogPosts < ApplicationRecord
     validates :title , presence: true
     validates :body , presence: true
 
-    scope :draft, ->{where(published_at: nil)}
-    scope :published, ->{where("published_at <=?",Time.current)}
-    scope :scheduled, ->{where("published_at > ?",Time.current)}
+    scope :draft, -> { where(published_at: nil)}
+    scope :published, -> { where("published_at <=?",Time.current)}
+    scope :scheduled, => { where("published_at > ?",Time.current)}
 
 end
 
